7877ad1cf8d9072bea7370241593157641aa761d
DO2JMG <34538443+DO2JMG@users.noreply.github.com>
2021-08-05 22:24:58 +0200

    Add files via upload

--- include/config.h
+++ include/config.h
@@ -5,8 +5,8 @@
 #define LoRa_RST 14
 #define LoRa_DIO0 26
 
-#define LoRa_Frequency_TX 433775E3
-#define LoRa_Frequency_RX 433775E3
+#define LoRa_Frequency_TX 433775E3                    // TX Frequency
+#define LoRa_Frequency_RX 433775E3                    // RX Frequency
 #define LoRa_SpreadingFactor 12
 #define LoRa_SignalBandwidth 125000
 #define LoRa_CodingRate4 5
@@ -18,17 +18,18 @@
 #define LoRa_Oled_RST 16  
 
 // TTGO Heltec
-//#define LoRa_Oled_SDA 4                               // TTGO T-Beam Pin 21
-//#define LoRa_Oled_SCL 15                              // TTGO T-Beam Pin 22
-//#define LoRa_Oled_RST 16                              // TTGO T-Beam Pin 16
+//#define LoRa_Oled_SDA 4                             // TTGO T-Beam Pin 21
+//#define LoRa_Oled_SCL 15                            // TTGO T-Beam Pin 22
+//#define LoRa_Oled_RST 16                            // TTGO T-Beam Pin 16
 
 
-#define Lora_Screen_Width 128 
-#define LoRa_Screen_Height 64 
+#define Lora_Screen_Width 128                         // Screen Width  
+#define LoRa_Screen_Height 64                         // Screen Height
+#define Lora_Screen_Timeout 1                         // Screen Timeout in Minutes
 
 #define LoRa_str_call "NOCALL"                        // Call for Digipeater
 #define LoRa_str_Lat "0000.00N"                       // Latitude
-#define LoRa_str_Lon "000000.00E"                      // Longitude
+#define LoRa_str_Lon "00000.00E"                      // Longitude
 #define LoRa_str_Symbol "&"                           // Symbol
 #define LoRa_str_Overlay "L"                          // Overlay
 #define LoRa_str_Comment "LoRa Digipeater"            // Comment
